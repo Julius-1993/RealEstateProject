7e25a7c54f24a0efc443a592341f9f95035e6b2f
Aako Julius Muyiwa <aakojuliusoluwanifemi@gmail.com>
2024-03-24 17:13:38 +0100

    Create Signin Api routes

--- api/controller/user.auth.controller.js
+++ api/controller/user.auth.controller.js
@@ -1,15 +1,36 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-
+import { errorHandler } from "../utils/error.js";
+import  jwt  from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10); // Hash the password
   const newUser = User({ username, email, password: hashedPassword });
-  try{
+  try {
     await newUser.save();
     res.status(201).json("Account created successfully!");
-  } catch(error){
+  } catch (error) {
     next(error);
   }
-  
+};
+//AUTHETICATION OF USER LOGIN/SIGN-IN AFTER SUCCESSFUL REGISTRATIOIN
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Invalid Password"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+  res
+    .cookie("access_token", token, {
+      httpOnly: true,
+      expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+    })
+    .status(200)
+    .json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
