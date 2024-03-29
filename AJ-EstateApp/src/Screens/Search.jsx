3ee3d60a9c6370c5dcfef2f5fb6945a8c27b80e6
Aako Julius Muyiwa <aakojuliusoluwanifemi@gmail.com>
2024-03-30 00:43:00 +0100

    Complete Serach Form Ui

--- AJ-EstateApp/src/Screens/Search.jsx
+++ AJ-EstateApp/src/Screens/Search.jsx
@@ -100,33 +100,33 @@ export default function Search() {
     }
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const urlParams = new URLSearchParams();
-    urlParams.set('searchTerm', sidebardata.searchTerm);
-    urlParams.set('type', sidebardata.type);
-    urlParams.set('parking', sidebardata.parking);
-    urlParams.set('furnished', sidebardata.furnished);
-    urlParams.set('offer', sidebardata.offer);
-    urlParams.set('sort', sidebardata.sort);
-    urlParams.set('order', sidebardata.order);
-    const searchQuery = urlParams.toString();
-    navigate(`/search?${searchQuery}`);
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   const urlParams = new URLSearchParams();
+  //   urlParams.set('searchTerm', sidebardata.searchTerm);
+  //   urlParams.set('type', sidebardata.type);
+  //   urlParams.set('parking', sidebardata.parking);
+  //   urlParams.set('furnished', sidebardata.furnished);
+  //   urlParams.set('offer', sidebardata.offer);
+  //   urlParams.set('sort', sidebardata.sort);
+  //   urlParams.set('order', sidebardata.order);
+  //   const searchQuery = urlParams.toString();
+  //   navigate(`/search?${searchQuery}`);
+  // };
 
-  const onShowMoreClick = async () => {
-    const numberOfListings = listings.length;
-    const startIndex = numberOfListings;
-    const urlParams = new URLSearchParams(location.search);
-    urlParams.set('startIndex', startIndex);
-    const searchQuery = urlParams.toString();
-    const res = await fetch(`/api/listing/get?${searchQuery}`);
-    const data = await res.json();
-    if (data.length < 9) {
-      setShowMore(false);
-    }
-    setListings([...listings, ...data]);
-  };
+  // const onShowMoreClick = async () => {
+  //   const numberOfListings = listings.length;
+  //   const startIndex = numberOfListings;
+  //   const urlParams = new URLSearchParams(location.search);
+  //   urlParams.set('startIndex', startIndex);
+  //   const searchQuery = urlParams.toString();
+  //   const res = await fetch(`/api/listing/get?${searchQuery}`);
+  //   const data = await res.json();
+  //   if (data.length < 9) {
+  //     setShowMore(false);
+  //   }
+  //   setListings([...listings, ...data]);
+  // };
   return (
     <div className='flex flex-col md:flex-row'>
       <div className='p-7  border-b-2 md:border-r-2 md:min-h-screen'>
