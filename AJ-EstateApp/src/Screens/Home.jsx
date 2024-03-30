4ed24e1f830c5b9190d01dc19e41dfdb99915070
Aako Julius Muyiwa <aakojuliusoluwanifemi@gmail.com>
2024-03-30 02:36:00 +0100

    Complete home page

--- AJ-EstateApp/src/Screens/Home.jsx
+++ AJ-EstateApp/src/Screens/Home.jsx
@@ -50,11 +50,11 @@ export default function Home() {
       {/* top */}
       <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
         <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
-          Find your next <span className='text-slate-500'>perfect</span>
+          Find your next <span className='text-violet-950'>perfect</span>
           <br />
           place with ease
         </h1>
-        <div className='text-gray-400 text-xs sm:text-sm'>
+        <div className='text-fuchsia-950 text-xs sm:text-sm'>
           AJ Estate is the best place to find your next perfect place to
           live.
           <br />
